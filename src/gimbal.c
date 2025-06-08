9f3efc722fdb33141d8b897bdb8225ef714b1bc1
maro159 <mrcm777@gmail.com>
2025-06-08 12:15:43 +0200

    fix gimbal initilization

--- src/gimbal.c
+++ src/gimbal.c
@@ -16,34 +16,32 @@ typedef const struct limits_t {
     float max_angle; // degrees
 } limits_t;
 
-static const limits_t yaw_limits = {
-    .min_angle = -45.0f,
-    .max_angle = 45.0f
-};
+static const limits_t yaw_limits = {.min_angle = -45.0f, .max_angle = 45.0f};
 
-static const limits_t pitch_limits = {
-    .min_angle = -15.0f,
-    .max_angle = 15.0f
-};
+static const limits_t pitch_limits = {.min_angle = -15.0f, .max_angle = 15.0f};
 
 int gimbal_init(void) {
-    if (!angle_servo_init(&gimbal_yaw, &servo_yaw, -95.0f, 95.0f)) {
+    if (angle_servo_init(&gimbal_yaw, &servo_yaw, -95.0f, 95.0f) != 0) {
         return -1;
     }
-    if (!angle_servo_init(&gimbal_pitch, &servo_pitch, -95.0f, 95.0f)) {
+    if (angle_servo_init(&gimbal_pitch, &servo_pitch, -95.0f, 95.0f) != 0) {
         return -1;
     }
     return 0;
 }
 
 int gimbal_set_yaw(float angle_deg) {
-    if (angle_deg < yaw_limits.min_angle) angle_deg = yaw_limits.min_angle;
-    if (angle_deg > yaw_limits.max_angle) angle_deg = yaw_limits.max_angle;
+    if (angle_deg < yaw_limits.min_angle)
+        angle_deg = yaw_limits.min_angle;
+    if (angle_deg > yaw_limits.max_angle)
+        angle_deg = yaw_limits.max_angle;
     return angle_servo_set_angle(&gimbal_yaw, angle_deg);
 }
 
 int gimbal_set_pitch(float angle_deg) {
-    if (angle_deg < pitch_limits.min_angle) angle_deg = pitch_limits.min_angle;
-    if (angle_deg > pitch_limits.max_angle) angle_deg = pitch_limits.max_angle;
+    if (angle_deg < pitch_limits.min_angle)
+        angle_deg = pitch_limits.min_angle;
+    if (angle_deg > pitch_limits.max_angle)
+        angle_deg = pitch_limits.max_angle;
     return angle_servo_set_angle(&gimbal_pitch, angle_deg);
 }
